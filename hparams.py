c8fe8f93ed253b0ea63f47fffd37db63eeac79cf
Pravin10 <66985376+Pravin1001@users.noreply.github.com>
2023-04-12 16:08:10 +0530

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -67,12 +67,11 @@ hparams = HParams(
 	# Limits
 	min_level_db=-100,
 	ref_level_db=20,
-	fmin=55,
-	min=55,
+	mel_fmin=55,
 	# Set this to 55 if your speaker is male! if female, 95 should help taking off noise. (To 
 	# test depending on dataset. Pitch info: male~[65, 260], female~[100, 525])
-	fmax=7600,
-	max=7600, # To be increased/reduced depending on data.
+	mel_fmax=7600,
+  	fmax=7600,  # To be increased/reduced depending on data.
 
 	###################### Our training parameters #################################
 	img_size=96,
